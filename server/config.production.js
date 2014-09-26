928684f2dea472a2ae94f8be9a3047d9b0a7409e
Rob Halff <rob.halff@gmail.com>
2014-09-26 04:08:11 +0200

    add openshift info

--- server/config.production.js
+++ server/config.production.js
@@ -1,11 +1,20 @@
+/***
+ *
+ * Environment variables for openshift:
+ *
+ * https://developers.openshift.com/en/managing-environment-variables.html
+ *
+ */
 module.exports = {
   restApiRoot: '/api',
-  host: '0.0.0.0',
-  port: 3000,
-  url: 'http://localhost:3000/',
-  storage: {
-    provider: 'amazon',
-    key: process.env.AMAZON_KEY,
-    keyId: process.env.AMAZON_KEY_ID
+  host: process.env.OPENSHIFT_IP,
+  port: process.env.OPENSHIFT_PORT,
+  url: 'http://' + process.env.OPENSHIFT_APP_DNS,
+  config: {
+    storage: {
+      provider: 'amazon',
+      key: process.env.AWS_SECRET_KEY,
+      keyId: process.env.AWS_ACCESS_KEY_ID
+    }
   }
 };
