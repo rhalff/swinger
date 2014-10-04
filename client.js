99bcbddfc2cd8dee0d8d56720ef85b48ecc89bec
Rob Halff <rob.halff@gmail.com>
2014-10-04 07:20:22 +0200

    client example

---
+++ client.js
@@ -0,0 +1,48 @@
+var loopback = require('loopback');
+var fs = require('fs');
+var client = loopback();
+
+var model = function(name, path) {
+
+  var fname = [path, name].join('/');
+  var definition = require(fname + '.json');
+  var model = loopback.createModel(definition);
+
+  if(fs.existsSync(fname + '.js')) {
+    require(fname + '.js')(model);
+  }
+
+  return model;
+
+};
+
+// hm ok what is received by the post.js is not
+// just only the json.
+// So what is received ?
+
+var post = model('post', './common/models');
+
+var remote = loopback.createDataSource({
+  connector: loopback.Remote,
+  url: 'http://swinger-chix.rhcloud.com/api'
+});
+
+client.model(post);
+
+post.attachTo(remote);
+
+/*
+// call the remote method
+post.sum(1, function(err, total) {
+  console.log('result:', err || total);
+});
+*/
+
+// call a built in remote method
+post.find(function(err, items) {
+  if(err) {
+    console.log(err);
+  } else {
+    console.log(items);
+  }
+});
